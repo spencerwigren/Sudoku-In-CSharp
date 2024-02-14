d1a2b3aec62529e4d763f9273edfe2f2fa91767d
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2024-02-14 10:39:26 -0700

    Board will now display in terminal

--- CSharp_Sudoku/Program.cs
+++ CSharp_Sudoku/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace CSharp_Sudoku;
+using System;
+
+public class Program
+{
+        public static void ShowBoard(int[,] board)
+        {
+            int newLine = 1;
+            int verticalLineCount = 0;
+            string horizontalLine = "  - - - + - - - + - - -";
+            string horizontalLineDivider = "  ------+-------+------";
+            int sideNumber = 2;
+            
+            Console.Write("\n  1 2 3   4 5 6   7 8 9 \n1 ");
+
+            foreach (var item in board)
+            {
+                // Settings Vertical lines
+                if (verticalLineCount is 3)
+                {
+                    Console.Write("| ");
+                    verticalLineCount = 1;
+                }
+                else
+                {
+                    verticalLineCount++;
+                }
+                
+                // Settings value 0 in board to blank
+                if (item == 0)
+                {
+                    Console.Write("0 ");
+                }
+                else
+                {
+                    Console.Write(item + " ");
+                }
+
+                // Starting New line info
+                if (newLine % 9 == 0)
+                {
+                    Console.WriteLine();
+                    if (sideNumber is 4 || sideNumber is 7)
+                    {
+                        Console.WriteLine(horizontalLineDivider);
+                        Console.Write(sideNumber++ + " ");
+                        // Settings here to start a new line count
+                        verticalLineCount = 0;
+                    }
+                    // Did this becuase it would write line 10
+                    // Where I need it to stop at 9
+                    else if (sideNumber < 10)
+                    {
+                        Console.WriteLine(horizontalLine);
+                        Console.Write(sideNumber++ + " ");
+                        // Settings here to start a new line count
+                        verticalLineCount = 0;
+                    }
+                }
+                newLine++;
+            }
+            // Helps with spacing in terminal
+            Console.WriteLine();
+        }
+
+
+        private static void Main()
+    {
+        int[,] Board = {
+            {5,3,0,0,7,0,0,0,0},
+            {6,0,0,1,9,5,0,0,0},
+            {0,9,8,0,0,0,0,6,0},
+            {8,0,0,0,6,0,0,0,3},
+            {4,0,0,8,0,3,0,0,1},
+            {7,0,0,0,2,0,0,0,6},
+            {0,6,0,0,0,0,2,8,0},
+            {0,0,0,4,1,9,0,0,5},
+            {0,0,0,0,8,0,0,7,9}
+            };
+
+        ShowBoard(Board);
+    }
+}
