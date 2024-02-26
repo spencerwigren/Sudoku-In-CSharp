f0cee103836a61afa647ae61a2b80ac79ebb5251
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2024-02-26 11:27:49 -0700

    Added Options for the game. Along with fixing UserModInput

--- CSharp_Sudoku/Program.cs
+++ CSharp_Sudoku/Program.cs
@@ -4,78 +4,95 @@ using System;
 
 public class Program
 {
-        public static (string?, string?) UserModInput() {
-            Console.Write("Vetical Cell Number: ");
-            string? VerticalNumber = Console.ReadLine();
+    public static Tuple<string?, string?, string?> UserModInput() 
+    {
+        Console.Write("Vetical Cell Number: ");
+        string? VerticalNumber = Console.ReadLine();
 
-            Console.Write("Horizonal Cell Number: ");
-            string? HorizonalNumner = Console.ReadLine();
-            // The ? infront of the string type is to help with nullable inputs
+        Console.Write("Horizonal Cell Number: ");
+        string? HorizonalNumner = Console.ReadLine();
+        // The ? infront of the string type is to help with nullable inputs
 
-            return (VerticalNumber, HorizonalNumner);
-        }
-        public static void ShowBoard(int[,] board)
+        Console.Write("New Value: ");
+        string? NewValue = Console.ReadLine();
+
+        Tuple<string?, string?, string?> ModValues = Tuple.Create(VerticalNumber, HorizonalNumner, NewValue);
+
+        return ModValues;
+    }
+    
+    public static void ShowBoard(int[,] board)
+    {
+        int newLine = 1;
+        int verticalLineCount = 0;
+        string horizontalLine = " | - - - + - - - + - - - |";
+        string horizontalLineDivider = " |-------+-------+-------|";
+        int sideNumber = 2;
+        
+        Console.Write("\n   1 2 3   4 5 6   7 8 9\n +-------+-------+-------+\n1| ");
+
+        foreach (var item in board)
         {
-            int newLine = 1;
-            int verticalLineCount = 0;
-            string horizontalLine = " | - - - + - - - + - - - |";
-            string horizontalLineDivider = " |-------+-------+-------|";
-            int sideNumber = 2;
-            
-            Console.Write("\n   1 2 3   4 5 6   7 8 9\n +-------+-------+-------+\n1| ");
-
-            foreach (var item in board)
+            // Settings Vertical lines
+            if (verticalLineCount is 3)
             {
-                // Settings Vertical lines
-                if (verticalLineCount is 3)
-                {
-                    Console.Write("| "); // Want two space
-                    verticalLineCount = 1;
-                }
-                else
-                {
-                    verticalLineCount++;
-                }
-                
-                // Settings value 0 in board to blank
-                if (item == 0)
-                {
-                    Console.Write(" "); // replacing all 0 in the list to blank
-                }
-                else
-                {
-                    Console.Write(item);
-                }
-                
-                // Starting New line info
-                if (newLine % 9 == 0)
-                {
-                    Console.Write(" |"); // Want no space
-                    Console.WriteLine();
-                    if (sideNumber is 4 || sideNumber is 7)
-                    {
-                        Console.WriteLine(horizontalLineDivider);
-                        Console.Write(sideNumber++ + "|"); // Want one space
-                        // Settings here to start a new line count
-                        verticalLineCount = 0;
-                    }
-                    // Did this becuase it would write line 10
-                    // Where I need it to stop at 9
-                    else if (sideNumber < 10)
-                    {
-                        Console.WriteLine(horizontalLine);
-                        Console.Write(sideNumber++ + "|"); // Want one space
-                        // Settings here to start a new line count
-                        verticalLineCount = 0;
-                    }
-                }
-                newLine++;
-                Console.Write(" ");
+                Console.Write("| "); // Want two space
+                verticalLineCount = 1;
             }
-            // Helps with spacing in terminal
-            Console.WriteLine("+-------+-------+-------+\n");
+            else
+            {
+                verticalLineCount++;
+            }
+            
+            // Settings value 0 in board to blank
+            if (item == 0)
+            {
+                Console.Write(" "); // replacing all 0 in the list to blank
+            }
+            else
+            {
+                Console.Write(item);
+            }
+            
+            // Starting New line info
+            if (newLine % 9 == 0)
+            {
+                Console.Write(" |"); // Want no space
+                Console.WriteLine();
+                if (sideNumber is 4 || sideNumber is 7)
+                {
+                    Console.WriteLine(horizontalLineDivider);
+                    Console.Write(sideNumber++ + "|"); // Want one space
+                    // Settings here to start a new line count
+                    verticalLineCount = 0;
+                }
+                // Did this becuase it would write line 10
+                // Where I need it to stop at 9
+                else if (sideNumber < 10)
+                {
+                    Console.WriteLine(horizontalLine);
+                    Console.Write(sideNumber++ + "|"); // Want one space
+                    // Settings here to start a new line count
+                    verticalLineCount = 0;
+                }
+            }
+            newLine++;
+            Console.Write(" ");
         }
-        private static void Main()
+        // Helps with spacing in terminal
+        Console.WriteLine("+-------+-------+-------+\n");
+    }
+        
+    public static void ShowOptions()
+    {
+            Console.WriteLine("Options: \nShow Options again - S");
+            Console.WriteLine("Display Baord - D");
+            Console.WriteLine("Edit the Board - E");
+            Console.WriteLine("Load File - F");
+            Console.WriteLine("End Game - G\n");
+    }
+    
+    private static void Main()
     {
         int[,] Board = {
             {5,3,0,0,7,0,0,0,0},
@@ -90,5 +107,6 @@ public class Program
             };
 
         ShowBoard(Board);
+        ShowOptions();
     }
 }
